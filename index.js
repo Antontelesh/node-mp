b662f10c965fe235cef36030f21457fb73a7e3a8
Anton Telesh <telesh91@gmail.com>
2017-09-23 15:19:27 +0300

    homework(2): implemented DirWatcher to watch for directory changes

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const path = require('path')
 require('babel-register')
 require('./main')
 const Importer = require('./importer')
+const DirWatcher = require('./dirwatcher')
 
 const importer = new Importer()
+const dirWatcher = new DirWatcher()
+const dataPath = path.relative(__dirname, './data')
 
-importer.import(path.relative(__dirname, './data/MOCK_DATA.csv'))
-  .then(
-    data => { console.log('data', data) },
-    error => { console.log('error', data) },
-  )
+dirWatcher.on('dirwatcher:changed', (files) => {
+  if (files.length) {
+    importer.import(dataPath + path.sep + files[0])
+      .then(
+        file => { console.log(file) },
+        error => { console.log(error) },
+      )
+  }
+})
+
+dirWatcher.watch(dataPath, 5000)
