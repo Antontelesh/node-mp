622ad87d178f79aa12dbbf01a3b58c7ebee7dff6
Anton Telesh <telesh91@gmail.com>
2017-10-05 12:26:10 +0300

    homework(3-streams): command-based API, transform-file command

--- utils/streams.js
+++ utils/streams.js
@@ -3,6 +3,8 @@
 const yargs = require('yargs')
 const fs = require('fs')
 const path = require('path')
+const throught2 = require('through2')
+const parse = require('csv-parse')
 
 const isCLI = require.main === module
 
@@ -10,32 +12,68 @@ const inputOutput = (_path) =>
   fs.createReadStream(path.relative(process.cwd(), _path))
     .pipe(process.stdout)
 
-const perform = ({ action, path }) => {
-  switch (action) {
-    case 'io': return inputOutput(path)
-    default: throw new Error(`Unknown action "${action}"`)
-  }
-}
+const transformFile = (_path) =>
+  fs.createReadStream(path.relative(process.cwd(), _path))
+    .pipe(parse({
+      columns: true,
+    }))
+    .pipe(throught2.obj(function (chunk, enc, cb) {
+      this.push(JSON.stringify(chunk, null, 2))
+      cb()
+    }))
+    .pipe(process.stdout)
 
 const argv = yargs
   .options({
-    'a': {
-      alias: 'action',
-      choices: [ 'io', 'transform-file', 'transform', 'bundle-css' ],
-      describe: 'An action to perform',
-      demandOption: true,
-    },
-    'p': {
-      alias: 'path',
-      describe: 'An action to perform',
-      demandOption: true,
-    },
-    'h': {
-      alias: 'help',
+    'help': {
+      alias: 'h'
     },
   })
+  .command(
+    'io',
+    'Read the file',
+    {
+      file: {
+        alias: 'f',
+        describe: 'path to the file',
+        normalize: true,
+        demandOption: true,
+      },
+    },
+    (argv) => inputOutput(argv.file)
+  )
+  .command(
+    'transform-file',
+    'Parse CSV file as JSON',
+    {
+      file: {
+        alias: 'f',
+        describe: 'path to the file',
+        normalize: true,
+        demandOption: true,
+      },
+    },
+    (argv) => transformFile(argv.file)
+  )
+  .command(
+    'transform',
+    'Parse CSV input as JSON',
+    {},
+    () => transform()
+  )
+  .command(
+    'bundle-css',
+    'Bundles all css files in the folder into one file',
+    {
+      path: {
+        alias: 'p',
+        describe: 'directory with css files to bundle',
+        normalize: true,
+        demandOption: true,
+      }
+    },
+    (argv) => bundleCss(path)
+  )
   .locale('en')
   .version(false)
   .argv
-
-perform(argv)
